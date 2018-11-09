932421b1bf473d8e3ff2a1ff710ba7e20b0af162
PSzczepanski1996 <p.szczepanski996@gmail.com>
2018-11-09 20:18:23 +0100

    Add kenfetch implementation

--- lib/cmd.c
+++ lib/cmd.c
@@ -9,14 +9,70 @@ int strcmp(const char* s1, const char* s2){
     return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
+void kenfetch(){
+    terminal_writestring("\n**********");
+        terminal_writestring("                            ");
+        terminal_setcolor(VGA_COLOR_LIGHT_MAGENTA);
+        terminal_writestring("hoshi@devkit\n");
+        terminal_setcolor(VGA_COLOR_LIGHT_GREY);
+        terminal_writestring(" *      *");
+        terminal_writestring("                             ");
+        terminal_setcolor(VGA_COLOR_LIGHT_BROWN);
+        terminal_writestring("OS: ");
+        terminal_setcolor(VGA_COLOR_WHITE);
+        terminal_writestring("KenOS ver 0.001 i686\n");
+        terminal_setcolor(VGA_COLOR_LIGHT_GREY);
+        terminal_writestring("  *    *");
+        terminal_writestring("                              ");
+        terminal_setcolor(VGA_COLOR_LIGHT_BROWN);
+        terminal_writestring("Shell: ");
+        terminal_setcolor(VGA_COLOR_WHITE);
+        terminal_writestring("Simpell\n");
+        terminal_setcolor(VGA_COLOR_LIGHT_GREY);
+        terminal_writestring("   *  *");
+        terminal_writestring("                               ");
+        terminal_setcolor(VGA_COLOR_LIGHT_BROWN);
+        terminal_writestring("Packages: ");
+        terminal_setcolor(VGA_COLOR_WHITE);
+        terminal_writestring("0\n");
+        terminal_setcolor(VGA_COLOR_LIGHT_GREY);
+        terminal_writestring("  ******");
+        terminal_writestring("                              ");
+        terminal_writestring("\n");
+        terminal_writestring("  * ** *");
+        terminal_writestring("                              ");
+        terminal_setcolor(VGA_COLOR_LIGHT_BROWN);
+        terminal_writestring("Colors: ");
+        terminal_setcolor(VGA_COLOR_LIGHT_GREY);
+        terminal_writestring("\n");
+        terminal_writestring("  ******");
+        terminal_writestring("                              ");
+        terminal_setcolor(VGA_COLOR_LIGHT_BLUE);
+        terminal_putchar(219);
+        terminal_setcolor(VGA_COLOR_LIGHT_GREEN);
+        terminal_putchar(219);
+        terminal_setcolor(VGA_COLOR_LIGHT_CYAN);
+        terminal_putchar(219);
+        terminal_setcolor(VGA_COLOR_LIGHT_RED);
+        terminal_putchar(219);
+        terminal_setcolor(VGA_COLOR_LIGHT_MAGENTA);
+        terminal_putchar(219);
+        terminal_setcolor(VGA_COLOR_LIGHT_BROWN);
+        terminal_putchar(219);
+        terminal_writestring("\n");
+}
+
 void execute(unsigned char *command){
     terminal_setcolor(VGA_COLOR_CYAN);
     if(!strcmp(command, "help")){
-        terminal_writestring("\nCommands: help, shell, user, osinfo, cls, reboot\n");
+        terminal_writestring("\nCommands: help, shell, user, kenfetch, osinfo, cls, reboot\n");
     } else if(!strcmp(command, "shell")){
         terminal_writestring("\nYou are using Simpell, that means simple shell\n");
     } else if(!strcmp(command, "user")){
         terminal_writestring("\nUser: Hoshi\n");
+    } else if(!strcmp(command, "kenfetch")){
+        terminal_setcolor(VGA_COLOR_LIGHT_GREY);
+        kenfetch();
     } else if(!strcmp(command, "osinfo")){
         terminal_writestring("\nKenOS, developed in mistery environment\n");
     } else if(!strcmp(command, "reboot")){
