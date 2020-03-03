b48840a18ed60c7425cb7cc73f9f47e2cce35318
PSzczepanski1996 <p.szczepanski996@gmail.com>
2020-03-03 21:30:55 +0100

    Set default timeout on GRUB, integrate serial port I/O into discord using new tool called SerialCord, shify run script, .gitignore fixes

--- misc/SerialCord/main.py
+++ misc/SerialCord/main.py
@@ -2,14 +2,19 @@
 from disconf import *
 import pexpect
 import discord
+import sys
 from discord.ext import commands
 desc = "SerialCord MikserCompany 2020"
 bot = commands.Bot(command_prefix="/", description=desc)
-kenOS = pexpect.spawn('qemu-system-i386 -nographic -serial stdio ../../kenos.iso')
+kenOS = pexpect.spawn('qemu-system-i386 -serial stdio ../../kenos.iso')
+kenOS.logfile_read = sys.stdout.buffer
 
 @bot.command()
 async def kenify(ctx, arg, *args):
-    kenOS.sendline(ctx.message.content[8:])
-    await ctx.send(kenOS.read().decode('utf-8'))
+    print(ctx.message.content[8:])
+    kenOS.send(ctx.message.content[8:])
+    kenOS.expect('kenOS: ')
+    response = kenOS.before
+    await ctx.send(response)
 
 bot.run(token)
