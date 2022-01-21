73391de264f01fe0b76a85facb842c18ec766529
PSzczepanski1996 <p.szczepanski996@gmail.com>
2022-01-21 10:41:04 +0100

    Change desc of SerialCord

--- misc/SerialCord/main.py
+++ misc/SerialCord/main.py
@@ -6,7 +6,7 @@ import time
 from discord.ext import commands
 from json_utils import read_json_file
 
-desc = "SerialCord MikserCompany 2020"
+desc = "SerialCord 2022"
 bot = commands.Bot(command_prefix="/", description=desc)
 config = read_json_file('config.json')
 kenOS = pexpect.spawn('qemu-system-i386 -serial stdio -drive file=../../kenos.iso,index=0,media=disk,format=raw')
