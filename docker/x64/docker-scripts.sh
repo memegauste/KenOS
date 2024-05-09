6ef123bc9eec73fd068250b532551ed66efd55c3
PSzczepanski1996 <p.szczepanski996@gmail.com>
2024-05-09 09:26:46 +0200

    Optimize kenOS ARM image, distinct ARM from building by now

--- docker/x64/docker-scripts.sh
+++ docker/x64/docker-scripts.sh
@@ -2,4 +2,4 @@ apt-get update
 apt-get install -y software-properties-common qemu-system-x86 gcc
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get install -y python3.12 python3.12-dev python3.12-venv
-#/bin/bash -c "make -j$(nproc) -k"
+/bin/bash -c "make -j$(nproc) -k"
